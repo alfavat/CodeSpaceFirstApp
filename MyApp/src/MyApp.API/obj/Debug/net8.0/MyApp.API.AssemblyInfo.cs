5c6c7e2a26f6d8a71876ab2b00ed9b67688327e8
alfavat <alfavat@gmail.com>
2025-12-27 16:55:18 +0000

    added register page

--- MyApp/src/MyApp.API/obj/Debug/net8.0/MyApp.API.AssemblyInfo.cs
+++ MyApp/src/MyApp.API/obj/Debug/net8.0/MyApp.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+346ae04693f2d6c33b888b86d64908d21b98da27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2641790e0370eb10102c610ca50a1d360547dae1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
